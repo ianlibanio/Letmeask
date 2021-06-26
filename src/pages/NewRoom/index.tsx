5b3c3e6f94db3478f22643b12f4266830ed6b388
Ian Libânio <huetroll1@gmail.com>
2021-06-26 17:21:01 -0300

    refactor: change new room creation redirect

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -39,7 +39,7 @@ export function NewRoom() {
       authorId: user?.id,
     });
 
-    history.push(`/rooms/${firebaseRoom.key}`);
+    history.push(`/admin/rooms/${firebaseRoom.key}`);
   }
 
   return (
